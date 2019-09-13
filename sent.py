5684151ee887f2c31ba89f3ffa1679027dea0bee
Xavi55 <evinxavier5@gmail.com>
2019-09-12 21:28:11 -0400

    sentiment fixes

--- sent.py
+++ sent.py
@@ -2,49 +2,57 @@
 import requests
 from bs4 import BeautifulSoup
 from textblob import TextBlob
-#import nltk #heroku stuff
-#nltk.download('punkt') #heroku stuff
+import nltk #heroku stuff
+nltk.download('punkt') #heroku stuff
 
 def feels(stock):
-	base='https://www.reuters.com/'
-	url=requests.get(base+'/finance/stocks/overview/'+stock)
+	base='https://www.reuters.com'
+	url=requests.get(base+'/companies/'+stock+'/news')
 	doc=BeautifulSoup(url.content,'html.parser')
-	links=doc.find_all('div',{'class':'feature'})
+	news=doc.find_all('div',{'class':'FeedScroll-feed-container-106s7'})
+	if not len(news):
+		print('No articles were found')
+		return [['No articles were found','No articles were found','https://www.reuters.com'],['UNKWN']]
+	else:
+		items=news[0].find_all('div',{'class':'item'})[:7]
+		links={}
+		for item in items:
+			try:
+				if links[item.a.get_text()]:
+					continue		
+			except KeyError as e:
+				links[item.a.get_text()]=item.a.get('href')
+					#print(item.a.get('href'),item.a.get_text())
+	####
+	##
+	#
+		array=[]##[[good sentences , bad sentences , src link] , ['rating']]
+		pos=0
+		neg=0
+		good=[]
+		bad=[]
+		rate=""
 
-	array=[]
-	#articles={'good':[],'bad':[],'source':[]}
-	pos=0
-	neg=0
-	good=[]
-	bad=[]
-	rate=""
-
-	for i,link in enumerate(links):
-		if i%3==0:
-			
-			print ('Searching in article #%s'%(i)) #article number on page
-
-			inner=link.a.get('href')
-			url2=requests.get(base+inner)
+		for link in links:
+			#print ('Searching in : %s'%(link))
+			url2=requests.get(links[link])
 			page=BeautifulSoup(url2.content,'html.parser')
 			article=page.find('div',{'class':'StandardArticleBody_body'}).text
 			unwanted_text=page.findAll('div',{'class':'Image_caption'})#images have capitions
 			for words in unwanted_text:
-				article = article.replace(words.text,'')#remove caption
+					article = article.replace(words.text,'')#remove caption
 			
 			cut=article.find('-')+2
 			cut2=article.find('Reporting by')
 			article = article[cut:cut2]
 			sentences=TextBlob(article).sentences #frag article into sentences
 			
-			if len(sentences) == 0:
+			if len(sentences)==0:
 				continue
-
+			
 			for sentence in sentences:
-				#print sentence
-				#print sentence.sentiment
-				#print
-				
+				#print(sentence,'::',sentence.sentiment)
+
 				if sentence.sentiment.polarity >.3 or .1<sentence.sentiment.polarity<.3 and sentence.sentiment.subjectivity<.46:
 					#print sentence
 					pos+=1
@@ -55,51 +63,20 @@ def feels(stock):
 					#print sentence
 					neg+=1
 					bad.append(str(sentence))#.decode(encoding='ascii',errors='ignore')
-		
-			#print ('The number of pos sentences was : %s'%(pos))
-			#print ('The number of neg sentences was : %s'%(neg))
-	
-	
-			#if True:#pos>neg: 
-				#print ('this article was good b/c of : %s '%(good))
-				#print
-				#print ('this artile was bad b/c of : %s'%(bad))
-				#print
-				
-			'''
-			elif pos==neg:
-				print ('this article was fair b/c of : %s &\n%s'%(good,bad))
-			else:
-				print ('this artile was bad b/c of : %s'%(bad))
-			'''
-			
-			array.append([good,bad,base+inner])
-			#append good sentenees, bad, & the src link
-
-			#articles['good'].append(good)
-			#articles['bad'].append(bad)
-			#articles['source'].append(base+inner)
-			
-			
-			#pos=0
-			#neg=0
-			good =[]
+			array.append([good,bad,links[link]])
+			good=[]
 			bad=[]
-
-	print ('The number of pos sentences was : %s'%(pos))
-	print ('The number of neg sentences was : %s'%(neg))
-	
-	if pos==neg:
-		rate= "fair"
-	elif pos>neg:
-		rate= "good";
-	else:
-		rate="bad"		
-	array.append(rate)
-	
-	#print len(array)
-	return array
-
-
-#test the script
+		#print ('The number of pos sentences was : %s'%(pos))
+		#print ('The number of neg sentences was : %s'%(neg))
+		
+		if pos==neg:
+			rate= "fair"
+		elif pos>neg:
+			rate= "good";
+		else:
+			rate="bad"		
+		array.append(rate)
+		return array
+###testing
 #feels('MMM')
+
